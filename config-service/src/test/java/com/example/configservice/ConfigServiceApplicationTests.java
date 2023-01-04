5a7c21845178117f8a56a9b145c56a946fcafcea
ch <mkmsacc@outlook.com>
2023-01-04 19:32:13 +0700

    Add test

--- config-service/src/test/java/com/example/configservice/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/com/example/configservice/ConfigServiceApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.configservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ConfigServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void shouldDefaultConfigurationAvailable() {
+        var entity = restTemplate
+                .getForEntity("/application/default", Environment.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 
 }
