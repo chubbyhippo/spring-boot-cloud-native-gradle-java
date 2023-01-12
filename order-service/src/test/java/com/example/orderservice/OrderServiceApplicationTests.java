1caf2644140ceadf426c68a4eb026f7e06b7fba4
ch <mkmsacc@outlook.com>
2023-01-12 11:12:47 +0700

    Refactor OrderServiceApplicationTests

--- order-service/src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
@@ -9,43 +9,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 import reactor.core.publisher.Mono;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
-class OrderServiceApplicationTests {
-
-    @Container
-    static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15.1"));
+class OrderServiceApplicationTests extends AbstractTestContainersConfig {
 
     @Autowired
     private WebTestClient webTestClient;
 
     @MockBean
     private BookClient bookClient;
-
-    @DynamicPropertySource
-    static void postgresqlProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.r2dbc.url", OrderServiceApplicationTests::r2dbcUrl);
-        registry.add("spring.r2dbc.username", postgresql::getUsername);
-        registry.add("spring.r2dbc.password", postgresql::getPassword);
-        registry.add("spring.flyway.url", postgresql::getJdbcUrl);
-    }
-
-    private static String r2dbcUrl() {
-        return String.format("r2dbc:postgresql://%s:%s/%s", postgresql.getHost(),
-                postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT), postgresql.getDatabaseName());
-    }
 
     @Test
     void shouldReturnOrdersWhenCallGet() {
